d66b3eac7b4a8aa016cfa3b39bb666ff27be5759
code4bau <65428717+code4bau@users.noreply.github.com>
2021-05-17 18:56:22 -0300

    commit initial

---
+++ JS/js/script.js
@@ -0,0 +1,10 @@
+/* let nombre = prompt("Ingresa su Nombre");
+
+let apellido = prompt("Ingrese su Apellido");
+
+alert(nombre  +  apellido )
+ */
+let nombre = prompt("Ingresa su Nombre");
+let nacimiento = prompt ("Ingrese su año de nacimiento")
+
+console.log(nombre  +   2021 - nacimiento)
