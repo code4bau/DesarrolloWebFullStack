f690245662ee1a351a72f559d37d439b58d55cfe
code4bau <65428717+code4bau@users.noreply.github.com>
2021-05-26 16:37:23 -0300

    commit

---
+++ javascript/unidad_2/actividad_obligatoria/js/script.js
@@ -0,0 +1,8 @@
+for (i = 11; i >= 9; i--) {
+  document.write("<br><b>La tabla del " + i + ":</b><br>");
+  for (j = 1; j <= 9 ; j--) {
+    document.write(i + " x " + j + ": ");
+    document.write(i * j);
+    document.write("<br>");
+  }
+}
